b69836cad6c608eec6d4ff4c99d11017ca8bb491
이소윤 <YOON@isoyun-ui-MacBook-Pro.local>
2019-06-08 21:27:52 -0400

    That's for only Pre-Midterm, not a final

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(() => {
+	console.log("Lee,Lin,Guzman midterm");
+
+/*trying to do*/	
+
+
+function viewText() { 
+  let soyoon = document.getElementById(".text"); 
+  if(soyoon.offsetHeight < 100) { 
+    document.getElementById(".text").style.height = ""; 
+    text01.value = "Read More"; 
+    } else { 
+    document.getElementById(".text").style.height = "100px"; 
+    text01.value = "Read Less"; 
+ 	} 
+
+ 	
+} 
+
+
+
+
+
+})();
